61f34625d95521c7cc15bbfb4c8120364345d654
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-08-28 21:45:07 -0500

    working on world js

--- main.js
+++ main.js
@@ -10,12 +10,6 @@ window.addEventListener("load", function () {
   canvas.width = GAME_WIDTH;
   canvas.height = GAME_HEIGHT;
 
-  function drawGrid() {
-    for (let row = 0; row < ROWS; row++) {
-      for (let col = 0; col < COLS; col++) {
-        ctx.strokeRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-      }
-    }
-  }
+  function drawGrid() {}
   drawGrid();
 });
--- scripts/world.js
+++ scripts/world.js
@@ -2,4 +2,11 @@ class World {
   constructor() {
     this.level1 = {};
   }
+  drawGrid() {
+    for (let row = 0; row < ROWS; row++) {
+      for (let col = 0; col < COLS; col++) {
+        ctx.strokeRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+      }
+    }
+  }
 }
