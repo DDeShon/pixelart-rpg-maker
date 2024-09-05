ce8d6192e7649eea15187e02f6c07b325179cc1a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-09-04 21:48:27 -0500

    working on gameObject.js file

--- scripts/gameObject.js
+++ scripts/gameObject.js
@@ -1,7 +1,7 @@
 import { TILE_SIZE } from "../main.js";
 
 export class GameObject {
-  constructor() {}
+  constructor(game, sprite) {}
 
   draw(ctx) {
     ctx.fillRect(0 * TILE_SIZE, 0 * TILE_SIZE, TILE_SIZE, TILE_SIZE);
