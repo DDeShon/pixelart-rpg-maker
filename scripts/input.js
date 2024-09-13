85a7e8e09726f3285b664cc3fa63b8f14abc3b87
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-09-13 02:34:22 -0500

    working on input.js file

--- scripts/input.js
+++ scripts/input.js
@@ -1,5 +1,12 @@
 export class Input {
   constructor() {
     this.keys = [];
+
+    window.addEventListener("keydown", (e) => {
+      console.log(e);
+    });
+    window.addEventListener("keyup", (e) => {
+      console.log(e);
+    });
   }
 }
