1c502041c680825c24659f8e669645fe3a1831e5
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-09-14 06:13:14 -0500

    working on input.js file

--- scripts/input.js
+++ scripts/input.js
@@ -1,3 +1,8 @@
+const LEFT = "LEFT";
+const RIGHT = "RIGHT";
+const UP = "UP";
+const DOWN = "DOWN";
+
 export class Input {
   constructor() {
     this.keys = [];
@@ -5,11 +10,14 @@ export class Input {
     window.addEventListener("keydown", (e) => {
       console.log("Pressed " + e.key);
       if (e.key === "ArrowUp" || e.key.toLowerCase() === "w") {
-        this.keyPressed();
+        this.keyPressed(UP);
       }
     });
     window.addEventListener("keyup", (e) => {
       console.log("Released " + e.key);
+      if (e.key === "ArrowUp" || e.key.toLowerCase() === "w") {
+        this.keyReleased(UP);
+      }
     });
   }
 
