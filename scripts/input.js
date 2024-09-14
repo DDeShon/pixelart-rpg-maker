94bc45b2bc08ea7b6bcdc2fa8698d62446f2f774
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-09-14 06:11:38 -0500

    working on input.js file

--- scripts/input.js
+++ scripts/input.js
@@ -4,9 +4,16 @@ export class Input {
 
     window.addEventListener("keydown", (e) => {
       console.log("Pressed " + e.key);
+      if (e.key === "ArrowUp" || e.key.toLowerCase() === "w") {
+        this.keyPressed();
+      }
     });
     window.addEventListener("keyup", (e) => {
       console.log("Released " + e.key);
     });
   }
+
+  keyPressed() {}
+
+  keyReleased() {}
 }
