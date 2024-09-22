1ec48ca5e027e75f7158d1b1ffc5a642dba2e48c
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-09-22 08:07:17 -0500

    working on hero.js file

--- scripts/hero.js
+++ scripts/hero.js
@@ -4,8 +4,14 @@ import { DOWN, LEFT, RIGHT, UP } from "./input.js";
 export class Hero extends GameObject {
   constructor({ game, sprite, position, scale }) {
     super({ game, sprite, position, scale });
+    this.speed = 2;
   }
   update() {
+    let nextX = this.destinationPosition.x;
+    let nextY = this.destinationPosition.y;
+
+    const distance = this.moveTowards(this.destinationPosition, this.speed);
+
     if (this.game.input.lastKey === UP) {
       this.position.y--;
     } else if (this.game.input.lastKey === DOWN) {
